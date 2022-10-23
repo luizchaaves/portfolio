c6e05f81b3c2378b6ac13dda2efb5f83a4f1234b
Luiz Otavio <luizotavio0106@outlook.com>
2022-10-23 01:42:38 -0300

    feat/skill bar creation

--- src/Components/skills/Skills.jsx
+++ src/Components/skills/Skills.jsx
@@ -6,38 +6,51 @@ import { SiReact } from 'react-icons/si';
 import './skills.css';
 
 const Skills = () => {
-  const skills = [
-    {
-      id: 1,
-      name: 'HTML',
-      description:
-        'HTML é uma linguagem baseada em marcação, onde marcamos os elementos para definir quais informações a página vai exibir',
-      icon: <SiHtml5 />,
-    },
-    {
-      id: 2,
-      name: 'CSS',
-      description:
-        'CSS é uma linguagem de folha de estilo composta por "camadas", criado com o propósito de estilizar as páginas HTML',
-      icon: <SiCss3 />,
-    },
-    {
-      id: 3,
-      name: 'JavaScript',
-      description:
-        'JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma',
-      icon: <SiJavascript />,
-    },
-    {
-      id: 4,
-      name: 'React',
-      description:
-        'React é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web',
-      icon: <SiReact />,
-    },
-  ];
+  const skills = React.useMemo(
+    () => [
+      {
+        id: 1,
+        name: 'HTML',
+        description:
+          'HTML é uma linguagem baseada em marcação, onde marcamos os elementos para definir quais informações a página vai exibir',
+        icon: <SiHtml5 />,
+        skill_level: '80%',
+      },
+      {
+        id: 2,
+        name: 'CSS',
+        description:
+          'CSS é uma linguagem de folha de estilo composta por "camadas", criado com o propósito de estilizar as páginas HTML',
+        icon: <SiCss3 />,
+        skill_level: '80%',
+      },
+      {
+        id: 3,
+        name: 'JavaScript',
+        description:
+          'JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma',
+        icon: <SiJavascript />,
+        skill_level: '70%',
+      },
+      {
+        id: 4,
+        name: 'React',
+        description:
+          'React é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web',
+        icon: <SiReact />,
+        skill_level: '65%',
+      },
+    ],
+    []
+  );
 
   const [iconActive, setIconActive] = React.useState(skills[0].id);
+
+  React.useEffect(() => {
+    var level_skill = document.getElementById('skill__level');
+    level_skill.style.width = skills[iconActive - 1].skill_level;
+    level_skill.dataset.content = skills[iconActive - 1].skill_level;
+  }, [skills, iconActive]);
 
   return (
     <section className="section" id="skills">
@@ -65,8 +78,17 @@ const Skills = () => {
               ))}
             </div>
           </div>
-          <div className="skills__content-description" id="divTexto">
-            {skills[iconActive - 1].description}
+          <div className="skill__container-level">
+            0%
+            <div className="skill__bar">
+              <div className="skill__level" id="skill__level"></div>
+            </div>
+            100%
+          </div>
+          <div>
+            <div className="skills__content-description" id="divTexto">
+              {skills[iconActive - 1].description}
+            </div>
           </div>
         </div>
       </div>
